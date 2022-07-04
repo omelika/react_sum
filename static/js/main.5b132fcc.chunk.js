7365a824ac432ad2795936b5db09af382f307403
Luidmyla Omelchenko <lui.omelika@gmail.com>
2022-07-04 14:48:59 +0300

    Updates

---
+++ static/js/main.5b132fcc.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact_sum=this.webpackJsonpreact_sum||[]).push([[0],{10:function(n,t,c){"use strict";c.r(t);c(1);var e=c(3),a=c.n(e),r=(c(8),c(0));var o=function(n){var t=n.a,c=void 0===t?0:t,e=n.b,a=void 0===e?0:e;return Object(r.jsx)("p",{children:"Sum of ".concat(c," and ").concat(a," is ").concat(c+a)})},s=function(){return Object(r.jsxs)(r.Fragment,{children:[o({a:2,b:3}),o({a:-5,b:5}),o({a:10}),o({b:5}),o({})]})};a.a.render(Object(r.jsx)(s,{}),document.getElementById("root"))},8:function(n,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.5b132fcc.chunk.js.map
